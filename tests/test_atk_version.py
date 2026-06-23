0ef89dd18f5e837b75ec51279de0a7348fad9656
Daniele Pigoli <antenati.toolkit.pro@gmail.com>
2026-06-23 22:16:23 +0200

    Prepare v3 RC2 hotfix metadata

--- tests/test_atk_version.py
+++ tests/test_atk_version.py
@@ -2,8 +2,8 @@ from src.atk_version import DISPLAY_VERSION, VERSION, is_newer_version
 
 
 def test_rc_version_identifiers_are_distinct():
-    assert VERSION == "3.0.0-rc1"
-    assert DISPLAY_VERSION == "3.0.0 RC1"
+    assert VERSION == "3.0.0-rc2"
+    assert DISPLAY_VERSION == "3.0.0 RC2"
 
 
 def test_final_release_is_newer_than_rc():
@@ -12,5 +12,6 @@ def test_final_release_is_newer_than_rc():
 
 def test_older_or_invalid_release_is_not_newer():
     assert is_newer_version("3.0.0-rc1") is False
+    assert is_newer_version("3.0.0-rc2") is False
     assert is_newer_version("2.9.9") is False
     assert is_newer_version("") is False
